a28184d3d82071c8f56e90d5fd2a5faddaba4906
Manuel Alejandro Preciado <90716908+alx016@users.noreply.github.com>
2022-09-06 10:46:52 -0500

    Update Experiment_2.m

--- Experiment_2.m
+++ Experiment_2.m
@@ -1,45 +1,48 @@
-%% Experiment_1
+%% Experiment_2
 % Audio recording & collection of data
+% This code is usefull to record 50 times and make an analysis of the
+% output signals. 
+%
+% Phrase = "la clase de señales es los lunes y los jueves"
 
-%  recording = audiorecorder(8000,16,1);
-%  disp('Start');
-%  recordblocking(recording, 5);
-%  disp('Stop');
-%  play(recording);
-%  recordingData = getaudiodata(recording);
-%  save ("audio", "recordingData");
+% recordingDataV = [];
+% for n=1:50
+%     recording = audiorecorder(8000,16,1);
+%     disp('Start');
+%     recordblocking(recording, 5);
+%     disp('Stop');
+%     %play(recording);
+%     recordingData = getaudiodata(recording);
+%     recordingDataV = [recordingDataV,recordingData];
+%     pause
+% end
+% save ('audio1_50', "recordingDataV");
 
-%% Part 2
+%% parte 2
 clear all
 close all
 clc
 
-% Loading Data
-load('audio.mat')
-n=length(recordingData);
-Fs = 8000;%Sampling Frequency
-t  = 0:5/(n-1):5; %Time vector
-
-% plot
-nexttile
-plot(recordingData)
-title('Recording Data')
+load('audio1_50.mat');
+n = length(recordingDataV);
+Fs = 8000;
+t  = 0:5/(n-1):5;
 
 nexttile
-plot(t,recordingData)
+plot(t,recordingDataV(:,2))% to select a specific column
 title('Recording Data vs Time')
 xlabel('t') ,ylabel('x(t)')
 
 % Stadistic Analisis
-m = mean(recordingData);%Media
-v = var(recordingData);%Varianza
-e = std(recordingData);%Desviación estandar
-a = skewness(recordingData);%Asimetría
-c = kurtosis(recordingData);%Curtosis
+m = mean(recordingDataV);%Media
+v = var(recordingDataV);%Varianza
+e = std(recordingDataV);%Desviación estandar
+a = skewness(recordingDataV);%Asimetría
+c = kurtosis(recordingDataV);%Curtosis
 d = v/e;% Dispersión = varianza / desviación estandar
 
 % Frequency Spectrum, data recovery and plot
-recordingDatafft = fft(recordingData);%Obtenemos la transformada
+recordingDatafft = fft(recordingDataV);%Obtenemos la transformada
 recordingDatafft_abs = abs(recordingDatafft/n);
 recordingDatafft_abs = recordingDatafft_abs(1:n/2+1);
 f = Fs*(0:n/2)/n;
@@ -48,3 +51,4 @@ nexttile
 plot(f,recordingDatafft_abs)
 title('Frequency vs X(t)')
 xlabel('Frequency'), ylabel('X(t)')
+
